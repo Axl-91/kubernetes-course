ad8e1b6a57bc3c3199118f878b247cfceaabfbdf
Axel Aparicio Rotermund <axelaparicior@gmail.com>
2025-08-13 00:55:09 -0300

    Added service and ingress to log_output, also change code

--- log_output/index.js
+++ log_output/index.js
@@ -1,11 +1,25 @@
+import http from 'node:http'
+
+const PORT = process.env.PORT || 3000;
 const randomUUID = crypto.randomUUID();
 
 const getOutput = () => {
   const date = new Date().toISOString();
 
-  console.log(`${date} ${randomUUID}`)
-
-  setTimeout(getOutput, 5000)
+  return `${date} ${randomUUID}`
 }
 
-getOutput();
+
+const server = http.createServer((req, res) => {
+  if (req.method === 'GET' && req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end(getOutput());
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not Found');
+  }
+});
+
+server.listen(PORT, () => {
+  console.log(`Server started in port ${PORT}`)
+})
